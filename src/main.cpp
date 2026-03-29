ea8892f0c68494b0e63e4634f2c86ee05a7a2918
alexborrazasm <alexborrazasm@gmail.com>
2026-03-29 17:00:28 +0200

    Added neoPixel

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include <Arduino.h>
 #include <TMP-API.h>
+#include <Adafruit_NeoPixel.h>
+
+#define PIN_LED      48 
+#define NUM_PIXELS    1
 
 TMP_RobotServer robotServer(80);
+Adafruit_NeoPixel rgbLed(NUM_PIXELS, PIN_LED, NEO_GRB + NEO_KHZ800);
 
 void setup_OTA();
 
@@ -16,6 +21,11 @@ void setup()
   
   robotServer.begin(WIFI_SSID, WIFI_PASS, false);
   robotServer.enableOTA(OTA_PASSWORD);
+
+  rgbLed.begin();
+  // Set the first pixel to off (black)
+  rgbLed.setPixelColor(0, rgbLed.Color(0, 0, 0));
+  rgbLed.show();
 }
 
 void loop() 
