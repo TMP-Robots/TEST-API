76f2ab16f916cd9d3fe6b7eff4d83b98ddfed928
alexborrazasm <alexborrazasm@gmail.com>
2026-03-29 20:50:31 +0200

    Update platformio.ini and main.cpp to support RGB LED color control

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,11 @@
 #define NUM_PIXELS    1
 
 TMP_RobotServer robotServer(80);
-Adafruit_NeoPixel rgbLed(NUM_PIXELS, PIN_LED, NEO_GRB + NEO_KHZ800);
 
-void setup_OTA();
+u_int8_t red = 0;
+u_int8_t green = 0;
+u_int8_t blue = 0;
+Adafruit_NeoPixel rgbLed(NUM_PIXELS, PIN_LED, NEO_GRB + NEO_KHZ800);
 
 void setup()
 {
@@ -21,14 +23,20 @@ void setup()
   
   robotServer.begin(WIFI_SSID, WIFI_PASS, false);
   robotServer.enableOTA(OTA_PASSWORD);
+  robotServer.registerVar("red", red);
+  robotServer.registerVar("green", green);
+  robotServer.registerVar("blue", blue);
 
   rgbLed.begin();
   // Set the first pixel to off (black)
   rgbLed.setPixelColor(0, rgbLed.Color(0, 0, 0));
   rgbLed.show();
+  
 }
 
 void loop() 
 {
   robotServer.update();
+  rgbLed.setPixelColor(0, rgbLed.Color(red, green, blue));
+  rgbLed.show();
 }
